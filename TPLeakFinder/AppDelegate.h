4ab21f77988b29ca3076c2eaecd8f80ec746f5ac
唐鹏 <tangpeng@icarbonx.com>
2018-03-28 15:20:03 +0800

    Initial Commit

---
+++ TPLeakFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TPLeakFinder
+//
+//  Created by 唐鹏 on 2018/3/28.
+//  Copyright © 2018年 ICX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TPLeakFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TPLeakFinder
+//
+//  Created by 唐鹏 on 2018/3/28.
+//  Copyright © 2018年 ICX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
